f38ceb197e7fd275dffba3888d215872f26ecd17
Christian White <christianwhite@Christian-Whites-MacBook-Pro.local>
2023-02-20 15:59:34 -0500

    gets json using regex for SWE II role

--- checkers/google.js
+++ checkers/google.js
@@ -11,14 +11,27 @@ v1.0
 3. Leverage email/text service?
 */
 
-import got from got;
+import got from 'got';
 
 const jobUrl = 'https://careers.google.com/api/v3/search/?distance=50&q=Software%20Engineer%20II';
 const getJobs = async()=> {
     let res = await got.get(jobUrl).json();
-    res = JSON.parse(res);
+	const matcher = /^Software Engineer II[^I]/;
+	res = filterByTitle(res.jobs, matcher);
     console.log(res);
 }
+
+//filter for jobs based on title
+const filterByTitle = (data, title) =>{
+	let filtered = [];
+	data.forEach(job => {
+		if(title.exec(job.title)){
+			filtered.push(job);
+		}
+	});
+	return filtered;
+}
+
 
 //test
 (async function(){
